2122ecabb37d8db36fd213a73d74126193970435
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-31 11:16:34 -0600

    Arreglar los parametros de las funciones
    Estaban mal echas desde elprincipio y ya estan
    arregladas.
    
    a
    sdf
    asd
    fa
    sdf
    asd
    fa
    sdf
    asdf
    as
    dfa
    sdf

--- funciones.h
+++ funciones.h
@@ -1,13 +1,13 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-double sume(double num1, num2);
+double sume(double num1, double num2);
 
-double resta(double num1, num2);
+double resta(double num1, double num2);
 
-double multiplica(double num1, num2);
+double multiplica(double num1, double num2);
 
-double divide(double num1, num2);
+double divide(double num1, double num2);
 
 
 
