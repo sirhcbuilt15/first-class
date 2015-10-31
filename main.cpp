6df7717b683c59b92e68573088d089ad7aa6dd4f
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-31 11:15:22 -0600

    Puro cuento de Patricia

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	std::cout << "Hola mundo, this is the begin... second though" << std::endl;
 	std::cout << "Adios mundo, this is the end..." << std::endl;
-	std::cout << "Adios mundo, heres come Patricia..." << std::endl;
+	std::cout << "Adios Patricia..." << std::endl;
 	return 0;
 }
 
