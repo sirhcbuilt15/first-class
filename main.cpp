bcfc02ba4766cbe344dd1960181e2ac6ac76a1f7
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-31 11:03:44 -0600

    Mensaje de patricia

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Hola mundo, this is the begin... second though" << std::endl;
 	std::cout << "Adios mundo, this is the end..." << std::endl;
+	std::cout << "Adios mundo, heres come Patricia..." << std::endl;
 	return 0;
 }
 
